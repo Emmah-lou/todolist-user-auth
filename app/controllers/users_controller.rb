bb07725c4cb8d291eaa0e4a961e61768678a1a4b
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-05-09 10:51:02 -0600

    added sessions table and attributes while setting up user auth, dealing with error of uerser exits on empty database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user && @user.save
+      render json: {
+        user: {
+          username: @user.username
+        }
+      }
+    else
+      render json: {
+        error: "Invalid username or password"
+      }
+    end
+  end
+
+
+
+  private
+  def user_params
+    params.permit(:username, :password)
   end
 end
